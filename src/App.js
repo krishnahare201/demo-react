0015ddb24c044dad1724c2b6d7e3c3990a2c953d
krishna201 <krishnahare201@gmail.com>
2021-10-31 21:19:44 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,50 +1,50 @@
-import React, { Component } from 'react'
 
-export default class App extends Component {
+const onMouseOver = event => {
+  const el = event.target;
+  let colorhex = [
+    "#7AF377",
+  ];
+  el.style.color = colorhex[0]
+};
 
-  render() {
-    const onMouseOver = event => {
-      const el = event.target;
-      let colorhex = [
-        "#7AF377",
-      ];
-      el.style.color = colorhex[0]
-    };
+const onMouseOut = event => {
+  const el = event.target;
+  let black = "#000000";
+  el.style.color = black;
+};
+const hocComponent = props => (
+  <div>
+    <h2> Inner HOC Component</h2>
+    <p>{props.name}</p>
+    <div>
+      <span
+        onMouseEnter={event => onMouseOver(event)}
+        onMouseOut={event => onMouseOut(event)}
+        style={{ width: 400 }}
+      >
+        Hover Me
+      </span>
+    </div>
+  </div>
+);
 
-    const onMouseOut = event => {
-      const el = event.target;
-      let black = "#000000";
-      el.style.color = black;
-    };
-    const hocComponent = props => (
-      <div>
-        <h2> Inner HOC Component</h2>
-        <p>{props.name}</p>
-        <div>
-          <span
-            onMouseEnter={event => onMouseOver(event)}
-            onMouseOut={event => onMouseOut(event)}
-            style={{ width: 400 }}
-          >
-            Hover Me
-          </span>
-        </div>
-      </div>
-    );
-
-    const hocCreator = WrappedComponent => {
-      return props => (
-        <div>
-          <h1>Outer HOC Component</h1>
-          <WrappedComponent {...props} />
-        </div>
-      );
-    };
-    const CustomHigherComponent = hocCreator(hocComponent);
-    return (
+const hocCreator = WrappedComponent => {
+  return props => (
+    <div>
+      <h1>Outer HOC Component</h1>
+      <WrappedComponent {...props} />
+    </div>
+  );
+};
+const CustomHigherComponent = hocCreator(hocComponent);
+function App() {
+  return (
+    <div >
       <div>
         <CustomHigherComponent name="display in Higher order component" isValid={false} />,
       </div>
-    )
-  }
+    </div>
+  );
 }
+
+export default App;
