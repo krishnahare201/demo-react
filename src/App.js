5efb1f51acea1ee90e4a6d1ab6a19ff1d1ad813c
krishna201 <krishnahare201@gmail.com>
2021-10-31 18:36:47 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  render() {
+    const onMouseOver = event => {
+      const el = event.target;
+      let colorhex = [
+        "#7AF377",
+      ];
+      el.style.color = colorhex[0]
+    };
+
+    const onMouseOut = event => {
+      const el = event.target;
+      let black = "#000000";
+      el.style.color = black;
+    };
+    const hocComponent = props => (
+      <div>
+        <h2> Inner HOC Component</h2>
+        <p>{props.name}</p>
+        <div>
+          <span
+            onMouseEnter={event => onMouseOver(event)}
+            onMouseOut={event => onMouseOut(event)}
+            style={{ width: 400 }}
+          >
+            Hover Me
+          </span>
+        </div>
+      </div>
+    );
+
+    const hocCreator = WrappedComponent => {
+      return props => (
+        <div>
+          <h1>Outer HOC Component</h1>
+          <WrappedComponent {...props} />
+        </div>
+      );
+    };
+    const CustomHigherComponent = hocCreator(hocComponent);
+    return (
+      <div>
+        <CustomHigherComponent name="display in Higher order component" isValid={false} />,
+      </div>
+    )
+  }
 }
-
-export default App;
